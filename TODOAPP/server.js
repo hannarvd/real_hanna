35e8541e05dd600cd1fcb631587a5cc56ccaea9e
hannarvd <114124115+hannarvd@users.noreply.github.com>
2022-10-16 22:58:38 +0900

    1

--- TODOAPP/server.js
+++ TODOAPP/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const Db = require('mongodb/lib/db');
 app.use(bodyParser.urlencoded({extended: true}));
 
 const MongoClient = require('mongodb').MongoClient;
-MongoClient.connect('mongodb+srv://hannalee:hannalee@cluster0.znocka6.mongodb.net/?retryWrites=true&w=majority', function(에러, client){
-    if (에러) return console.log(에러);
+
+
+var db;
+MongoClient.connect('mongodb+srv://hannalee:hannalee@cluster0.znocka6.mongodb.net/todoapp?retryWrites=true&w=majority', function(에러, client){
+    if (에러) return console.log(에러)
+    db = client.db('todoapp');
+
+    db.collection('post').insertOne({이름 : 'John', 나이 : 20}, function(에러, 결과){
+      console.log('저장완료');
+    });
+
     app.listen('8080', function(){
       console.log('listening on 8080')
     });
@@ -26,5 +36,8 @@ app.post('/add',function(req, res){
     res.send('전송완료');
     console.log(req.body.title);
     console.log(req.body.date);
+    db.collection('post').insertOne({제목 : req.body.title, 날짜: req.body.date}, function(에러, 결과){
+      console.log('저장완료');
+    });
 
 });
--- restaurant-reviews/backend/api/restaurants.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import RestaurantsCtrl from "./restaurants.controller.js"
-
-const router = express.Router()
-
-router.route("/").get((RestaurantsCtrl,apiGetRestaurants) => res.send("hello world"))
-
-export default router
